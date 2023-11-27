008fe8b9c7511284e8d534988fd3f28c65ef774b
Orlando Junior <134120820+orlandojsjunior@users.noreply.github.com>
2023-11-27 19:47:12 -0300

    Desafios 1 a 4
    README.md

---
+++ desafio 02.py
@@ -0,0 +1,5 @@
+dia = input("Qual dia voce nasceu? ")
+mes = input("E o mês? ")
+ano = input("Agora me fala o ano: ")
+
+print("Voce nasceu no dia ", dia,"de", mes, "de", ano, ". Correto?")
---
+++ desafio 03.py
@@ -0,0 +1,4 @@
+n1 = float(input("Digite um numero: "))
+n2 = float(input("Digite outro numero: "))
+s = n1 + n2
+print("A soma entre {} e {} é {}".format(n1, n2, s))
