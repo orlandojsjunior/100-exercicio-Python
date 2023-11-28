cdc6a067d06abd3a522c3fd6ee4c6afda955cd0f
Orlando Junior <orlandojsjunior@hotmail.com>
2023-11-27 23:57:24 -0300

    Exercício 4 – Dissecando uma Variável

---
+++ desafio 004.py
@@ -0,0 +1,6 @@
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(a))
+print('Só tem espaços? ', a.isspace())
+print('É um numero? ', a.isalnum())
+print('É alfabetico? ', a.isalpha())
+print('teste: ', a.isupper())
