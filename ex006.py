bb80f0ebcf1a25351de97a99c576453573224a7d
Orlando Junior <orlandojsjunior@hotmail.com>
2023-11-28 22:36:03 -0300

    Exercício Python 006 - Dobro, Triplo, Raiz Quadrada

---
+++ ex006.py
@@ -0,0 +1,7 @@
+
+nun = int(input('Digite o numero: '))
+dobro = nun * 2
+triplo = nun * 3
+raiz_quadrada = nun ** 2
+
+print('Esse e o seu dobro {} seu triplo {} e sua raiz quadrada {}'.format(dobro, triplo, raiz_quadrada))
