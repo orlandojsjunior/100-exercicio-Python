b845a1e1314836293314e5479004c7d5e9c4c4be
Orlando Junior <orlandojsjunior@hotmail.com>
2023-11-29 00:32:53 -0300

    Exercício Python 007 - Média Aritmética

---
+++ ex007.py
@@ -0,0 +1,15 @@
+# Exercicio Python 007: Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre a sua media.
+
+n1 = float(input("Digite a nota da AV1: "))
+n2 = float(input("Digite a nota da AV2: "))
+media = (n1 + n2) / 2
+
+print("Sua media e: {}".format(media))
+
+# Implementação de codigo | if-else para verificar se a média é maior ou igual a 7, e mostrar uma mensagem de aprovado ou reprovado.
+
+if media >= 7:
+    print("Parabens, voce foi aprovado!")
+else:
+    print("Infelizmente, voce foi reprovado.")
+
