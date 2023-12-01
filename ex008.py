bb9b6c48969a1c424d2ae1cf93e6f2c892d9bdad
Orlando Junior <orlandojsjunior@hotmail.com>
2023-12-01 00:14:38 -0300

    Exercício Python 008 - Conversor de Medidas

---
+++ ex008.py
@@ -0,0 +1,6 @@
+# Exercício Python 8: Escreva um programa que leia um valor em metros e o exiba convertido em centímetros e milímetros.
+
+medida = float(input("Qual a distancia em metros que deseja converter: "))
+cm = medida * 100
+mm = medida * 1000
+print("A conversão de {}mts para centimentros é {:.0f}cm e para milimentros é {:.0f}mm".format(medida, cm, mm))
